2f5fdeaad56ae62682c8e94f56c40208f3b0b861
Igor Nadj <igor.nadj@gmail.com>
2025-08-02 18:47:38 +1000

    Add RSF Zero

--- server/src/action-utils/client/isServerAction.ts
+++
@@ -1,9 +0,0 @@
-import { extname } from "./extname.ts";
-
-export const isServerAction = (id: string, code: string) => {
-  const ext = extname(id);
-  if (ext === ".ts") {
-    return code.startsWith("'use server'") || code.startsWith('"use server"');
-  }
-  return false;
-};
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>RSF Zero up and running!</StrictMode>,
+);
