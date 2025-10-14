9969e55c378539d3e6fef13bf653e81d50d74120
Igor Nadj <igor.nadj@gmail.com>
2025-10-14 18:16:17 +1100

    Swap ical package

--- src/utils/ical.ts
+++ src/utils/ical.ts
@@ -1,37 +1,31 @@
-import { createEvents, type EventAttributes } from "ics";
+import { generateIcsCalendar, type IcsCalendar, type IcsEvent } from "ts-ics";
 import type { Calendar, CalendarEvent } from "../types.ts";
 
 export function generateICalendar(calendar: Calendar, events: CalendarEvent[]): string {
-  const icalEvents: EventAttributes[] = events.map((event) => ({
+  const icalEvents: IcsEvent[] = events.map((event) => ({
     uid: event.uuid,
-    title: event.name,
-    start: [event.date.getFullYear(), event.date.getMonth() + 1, event.date.getDate()],
-    startInputType: "local",
-    duration: { hours: 1 }, // Default 1 hour duration
-    productId: "anycal/1.0.0",
-    calName: calendar.name,
-    created: [
-      event.created.getFullYear(),
-      event.created.getMonth() + 1,
-      event.created.getDate(),
-      event.created.getHours(),
-      event.created.getMinutes(),
-    ],
-    lastModified: [
-      event.lastModified.getFullYear(),
-      event.lastModified.getMonth() + 1,
-      event.lastModified.getDate(),
-      event.lastModified.getHours(),
-      event.lastModified.getMinutes(),
-    ],
+    summary: event.name,
+    start: {
+      date: event.date,
+    },
+    end: {
+      date: event.date,
+    },
+    stamp: {
+      date: event.created,
+    },
+    lastModified: {
+      date: event.date,
+    },
     sequence: event.sequence,
   }));
 
-  const { error, value } = createEvents(icalEvents);
+  const icalCalendar: IcsCalendar = {
+    version: "2.0",
+    prodId: "-//anycal//anycal 1.0.0//EN",
+    name: calendar.name,
+    events: icalEvents,
+  };
 
-  if (error) {
-    throw new Error(`Failed to generate iCal: ${error.message}`);
-  }
-
-  return value || "";
+  return generateIcsCalendar(icalCalendar);
 }
