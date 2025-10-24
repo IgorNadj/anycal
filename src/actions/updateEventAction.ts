9ab0c7c13587c8474d276b467ea8f14f2094ff0c
Igor Nadj <igor.nadj@gmail.com>
2025-10-24 14:21:26 +1100

    Fix update event

--- src/actions/updateEventAction.ts
+++ src/actions/updateEventAction.ts
@@ -3,11 +3,13 @@
 import { database } from "../database.ts";
 import { updateEvent } from "../sql/mutations.ts";
 import type { UpdateCalendarEvent } from "../types.ts";
+import { ok } from "../utils/validation.ts";
 
 export const updateEventAction = async (event: UpdateCalendarEvent) => {
-  return updateEvent(database, {
+  updateEvent(database, {
     ...event,
     lastModified: new Date(), // do this on the server in case client time is wrong
     sequence: event.sequence + 1,
   });
+  return ok({});
 };
--- src/hooks/useUpdateEvent.ts
+++ src/hooks/useUpdateEvent.ts
@@ -1,6 +1,6 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { useContext } from "react";
-import { createEventAction } from "../actions/createEventAction.ts";
+import { updateEventAction } from "../actions/updateEventAction.ts";
 import { AuthContext } from "../state/AuthContext.tsx";
 import type { UpdateCalendarEvent } from "../types.ts";
 import { NotLoggedInError } from "../utils/NotLoggedInError.ts";
@@ -8,13 +8,12 @@ import { useValidatedMutation } from "../utils/validation.ts";
 
 export const useUpdateEvent = () => {
   const queryClient = useQueryClient();
-
   const { userUuid } = useContext(AuthContext);
 
   return useValidatedMutation({
     validatedMutationFn: async (event: UpdateCalendarEvent) => {
       if (!userUuid) throw NotLoggedInError();
-      return createEventAction(event);
+      return updateEventAction(event);
     },
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: [userUuid, "events"] });
