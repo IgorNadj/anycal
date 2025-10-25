3b5f5f75bccf5d09ab04898dad627018eaa35e51
Igor Nadj <igor.nadj@gmail.com>
2025-10-25 14:50:59 +1100

    Add web grounding to LLM lookup

--- src/actions/fetchSuggestionsAction.ts
+++ src/actions/fetchSuggestionsAction.ts
@@ -1,8 +1,8 @@
 "use server";
 
-import type { Suggestion } from "../types.ts";
 import { GoogleGenAI } from "@google/genai";
 import { add, format, parseISO } from "date-fns";
+import type { Suggestion } from "../types.ts";
 
 const cache: Record<string, Suggestion[]> = {};
 
@@ -20,11 +20,27 @@ export const fetchSuggestionsAction = async (input: string): Promise<Suggestion[
 
   const ai = new GoogleGenAI({ apiKey });
 
+  const groundingTool = {
+    googleSearch: {},
+  };
+
+  const config = {
+    tools: [groundingTool],
+    systemInstruction: `You are an API endpoint for a client interface which is used for making 
+      new calendar events, sourced from the internet. A user will say a thing, and your job is 
+      to find when that thing is, and return the date. If the thing is ambiguous, return multiple 
+      suggestions, each with a title and date. The date should be in ISO8601 RFC3339 format. 
+      
+      Return only the json, nothing else. Don't return any events 
+      before ${format(add(new Date(), { days: 1 }), "yyyy-MM-dd")}. 
+      
+      The response structure should be: "{ suggestions: [{ title: string, date: string }] }". 
+      If there are no suggestions, return an empty array.`,
+  };
+
   const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    config: {
-      systemInstruction: `You are an API endpoint for a client interface which is used for making new calendar events, sourced from the internet. A user will say a thing, and your job is to find when that thing is, and return the date. If the thing is ambiguous, return multiple suggestions, each with a title and date. The date should be in ISO8601 RFC3339 format. Return only the json, nothing else. Don't return any events before ${format(add(new Date(), { days: 1 }), "yyyy-MM-dd")}. The response structure should be: "{ suggestions: [{ title: string, date: string }] }". If there are no suggestions, return an empty array.`,
-    },
+    model: "gemini-2.5-flash",
+    config,
     contents: input,
   });
 
@@ -32,7 +48,8 @@ export const fetchSuggestionsAction = async (input: string): Promise<Suggestion[
     throw new Error("LLM response empty");
   }
 
-  console.log("LLM Response: ", response.text);
+  // console.log("LLM Response: ", JSON.stringify(response));
+  // console.log("LLM Response text: ", response.text);
 
   const strippedText = response.text.replace("```json", "").replace("```", "");
 
