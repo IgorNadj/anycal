853143df35a8d6c9180aad069b54fda083a77ed7
Igor Nadj <igor.nadj@gmail.com>
2025-10-25 15:22:21 +1100

    Allow analytics script to load

--- rsf0-config.js
+++ rsf0-config.js
@@ -2,7 +2,7 @@
 export default {
   startStatic: {
     setHeaders: (res, path, stat) => {
-      res.setHeader("Content-Security-Policy", "script-src 'self' *.igornadj.io");
+      res.setHeader("Content-Security-Policy", "script-src 'self' *.dev.igornadj.io");
     },
   },
   routes: ["./src/routes/calendar-ics.ts"],
