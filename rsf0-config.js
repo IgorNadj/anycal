514238eeb7723375d7917d8988f8164a26e885d1
Igor Nadj <igor.nadj@gmail.com>
2025-10-25 14:36:41 +1100

    Allow analytics script to load

--- rsf0-config.js
+++ rsf0-config.js
@@ -1,4 +1,12 @@
 /** @type { import('rsf-zero').RsfZeroConfig } */
 export default {
+  startStatic: {
+    setHeaders: (res, path, stat) => {
+      res.setHeader(
+        "Content-Security-Policy",
+        "default-src 'self'; script-src *.igornadj.io",
+      );
+    },
+  },
   routes: ["./src/routes/calendar-ics.ts"],
 };
