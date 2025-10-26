e29cccfdf03165df7512c75549f8e1a55a95cdd0
Igor Nadj <igor.nadj@gmail.com>
2025-10-26 19:36:30 +1100

    Refactor: extract ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { createTheme, ThemeProvider } from "@mui/material";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -6,57 +5,12 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Main } from "./components/Main.tsx";
 import { AuthProvider } from "./providers/AuthProvider.tsx";
 import { StateProvider } from "./providers/StateProvider.tsx";
+import { ThemeProvider } from "./providers/ThemeProvider.tsx";
 import { getUserLocale } from "./utils.ts";
 
 const userLocale = getUserLocale();
 
 const queryClient = new QueryClient();
-
-const theme = createTheme({
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#ffcc00",
-    },
-    secondary: {
-      main: "#3b9bf8",
-    },
-    info: {
-      main: "#4ca4ee",
-    },
-    success: {
-      main: "#4ca4ee",
-    },
-  },
-  typography: {
-    fontFamily: ["Montserrat", "Roboto", "sans-serif"].join(","),
-    fontSize: 13,
-    h6: {
-      fontSize: "0.9rem",
-    },
-    h5: {
-      fontSize: "1rem",
-    },
-    h4: {
-      fontSize: "1.1rem",
-    },
-    h3: {
-      fontSize: "1.2rem",
-    },
-    h2: {
-      fontSize: "1.4rem",
-    },
-    h1: {
-      fontSize: "1.6rem",
-      fontWeight: 300,
-    },
-    body2: {
-      fontSize: 12,
-      fontFamily: "Lexend, Roboto, sans-serif",
-      fontWeight: 300,
-    },
-  },
-});
 
 export const App = () => {
   return (
@@ -64,7 +18,7 @@ export const App = () => {
       <StateProvider>
         <AuthProvider>
           <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={userLocale}>
-            <ThemeProvider theme={theme}>
+            <ThemeProvider>
               <Main />
               <ReactQueryDevtools initialIsOpen={false} />
             </ThemeProvider>
---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,56 @@
+import { createTheme, ThemeProvider as BaseThemeProvider } from "@mui/material";
+import { type ReactNode } from "react";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#ffcc00",
+    },
+    secondary: {
+      main: "#3b9bf8",
+    },
+    info: {
+      main: "#4ca4ee",
+    },
+    success: {
+      main: "#4ca4ee",
+    },
+  },
+  typography: {
+    fontFamily: ["Montserrat", "Roboto", "sans-serif"].join(","),
+    fontSize: 13,
+    h6: {
+      fontSize: "0.9rem",
+    },
+    h5: {
+      fontSize: "1rem",
+    },
+    h4: {
+      fontSize: "1.1rem",
+    },
+    h3: {
+      fontSize: "1.2rem",
+    },
+    h2: {
+      fontSize: "1.4rem",
+    },
+    h1: {
+      fontSize: "1.6rem",
+      fontWeight: 300,
+    },
+    body2: {
+      fontSize: 12,
+      fontFamily: "Lexend, Roboto, sans-serif",
+      fontWeight: 300,
+    },
+  },
+});
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  return <BaseThemeProvider theme={theme}>{children}</BaseThemeProvider>;
+};
