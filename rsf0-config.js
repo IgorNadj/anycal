8505bd5864301d9b912622b149d5941924c2a31e
Igor Nadj <igor.nadj@gmail.com>
2025-10-26 22:38:40 +1100

    Fix analytics

--- rsf0-config.js
+++ rsf0-config.js
@@ -1,12 +1,4 @@
 /** @type { import('rsf-zero').RsfZeroConfig } */
 export default {
-  startStatic: {
-    setHeaders: (res, path, stat) => {
-      res.setHeader(
-        "Content-Security-Policy",
-        "script-src 'self' analytics.dev.igornadj.io",
-      );
-    },
-  },
   routes: ["./src/routes/calendar-ics.ts"],
 };
